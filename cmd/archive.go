1e64052bb1274ed0ac29582d45f1ad89528ed9a3
Art P <fn3x@yandex.ru>
2024-05-22 18:40:10 +0400

    fix!: removed splitting into two commands: there could be new inserts by the time program reaches for the execution of the second command

--- cmd/archive.go
+++ cmd/archive.go
@@ -18,17 +18,7 @@ var archiveCmd = &cobra.Command{
 	Use:   "archive --table=table_name --where='where_clause' [--table=... --where='...' ...]",
 	Short: "Archive tables",
 	Long: `
-Archive tables using created config ('init' command) from source database to destination database with pt-archiver.
-
-It is split into two separate commands:
-
-    pt-archiver --no-delete ...
-
-and
-
-    pt-archiver --purge ...
-
-for safety measures.`,
+Archive tables using created config ('init' command) from source database to destination database with pt-archiver.`,
 	RunE: func(command *cobra.Command, args []string) error {
 		if err := viper.ReadInConfig(); err != nil {
 			return errors.New(fmt.Sprintf("%+v\n\n%s", err, "To create config file:\n  archive init"))
@@ -56,7 +46,6 @@ for safety measures.`,
 			args := []string{
 				"--progress",
 				"10",
-				"--no-delete",
 				"--share-lock",
 				"--commit-each",
 				"--statistics",
@@ -85,40 +74,6 @@ for safety measures.`,
 
 			cmd := exec.Command("pt-archiver", args...)
 			var stdout, stderr bytes.Buffer
-
-			cmd.Stdout = &stdout
-			cmd.Stderr = &stderr
-
-			if err := cmd.Run(); err != nil {
-				return errors.New(fmt.Sprintf("%s\n%s", err.Error(), stderr.String()))
-			}
-
-			fmt.Fprintln(os.Stderr, stderr.String())
-			fmt.Fprintln(os.Stdout, stdout.String())
-
-			deleteArgs := []string{
-				"--progress",
-				"10",
-				"--purge",
-				"--primary-key-only",
-				"--commit-each",
-				"--statistics",
-				"--why-quit",
-				"--socket",
-				viper.GetString("socket"),
-				"--source",
-				fmt.Sprintf("h=%s,D=%s,P=%s,u=%s,p=%s,t=%s",
-					viper.GetString("source.host"),
-					viper.GetString("source.db"),
-					viper.GetString("source.port"),
-					viper.GetString("source.user"),
-					viper.GetString("source.password"),
-					tables[i]),
-				"--where",
-				fmt.Sprintf("'%s'", wheres[i]),
-			}
-
-			cmd = exec.Command("pt-archiver", deleteArgs...)
 
 			cmd.Stdout = &stdout
 			cmd.Stderr = &stderr
